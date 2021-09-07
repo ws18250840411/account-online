7a9e61a6ec62fadd9c361053b9ec1cca753b2a5b
wangws <wangws@akulaku.com>
2021-09-07 15:14:28 +0800

    feat: a

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="header">
-          🚀 Vite + Reactasd + Typescript 🤘 & <br />
+          🚀 Vite + Reactasdasd + Typescript 🤘 & <br />
           Eslint 🔥+ Prettier
         </p>
 
